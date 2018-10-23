755bd4b9adadb07452b268bb1b3572c01d02a856
张立享 <zlx_ooo@126.com>
2018-10-23 18:23:52 +0800

    修改白皮书文件位置，修改配置文件上传服务器路径。

--- src/main/java/com/wchm/website/config/WebMvcConfigurer.java
+++ src/main/java/com/wchm/website/config/WebMvcConfigurer.java
@@ -8,29 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
-    @Value("${wchm.update-image-relative}")
+    @Value("${wchm.update-relative}")
     private String relative;
 
-    @Value("${wchm.update-image-absolutely}")
+    @Value("${wchm.update-absolutely}")
     private String absolutely;
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        /**
-//         * @Description: 对文件的路径进行配置, 创建一个虚拟路径/templates/** ，即只要在<img src="/templates/picName.jpg" />便可以直接引用图片
-//         *这是图片的物理路径  "file:/+本地图片的地址"
-//         */
-//        registry.addResourceHandler("/templates/**").addResourceLocations
-//                ("file:/E:/IdeaProjects/gaygserver/src/main/resources/static/");
-////        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/static/");
-//        super.addResourceHandlers(registry);
-//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 和页面有关的静态目录都放在项目的static目录下
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        // 上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/d3cf0281-bb7f-40e0-ab77-406db95ccf2c.jpg
+        // 上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/xxx.jpg
         // 其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
         registry.addResourceHandler(relative + "**").addResourceLocations("file:" + absolutely);
     }
