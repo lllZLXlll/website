5d5df8a858fed25f0b877f2f10eeee4526b94b0d
张立享 <zlx_ooo@126.com>
2018-11-01 09:29:09 +0800

    修改配置文件。

--- src/main/java/com/wchm/website/config/WebAppConfig.java
+++ src/main/java/com/wchm/website/config/WebAppConfig.java
@@ -1,18 +1,49 @@
 package com.wchm.website.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wchm.website.interceptor.AuthorityInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.MultipartConfigElement;
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
- * spring boot 配置文件上传大小限制
+ * WEB应用配置类
  */
 @Configuration
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
+    @Value("${wchm.update-relative}")
+    private String relative;
+
+    @Value("${wchm.update-absolutely}")
+    private String absolutely;
+
+    /**
+     * 配置静态资源路径
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 和页面有关的静态目录都放在项目的static目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // 上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/xxx.jpg
+        // 其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
+        registry.addResourceHandler(relative + "**").addResourceLocations("file:" + absolutely);
+    }
+
+    /**
+     * 配置上传文件大小限制
+     */
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
@@ -22,4 +53,47 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         factory.setMaxRequestSize("10MB");
         return factory.createMultipartConfig();
     }
+
+    /**
+     * 解决乱码
+     */
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter messageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(getObjectMapper());
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        // 解决中文乱码
+        converters.add(responseBodyConverter());
+        // 解决 添加解决中文乱码后 上述配置之后，返回json数据直接报错 500：no convertter for return value of type
+        converters.add(messageConverter());
+    }
+
+    /**
+     * 配置登录拦截器拦截的路径
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        // 配置拦截路径，和不拦截路径
+        registry.addInterceptor(new AuthorityInterceptor()).addPathPatterns("/admin/**").excludePathPatterns("/admin/login/**");
+
+    }
+
+
 }
